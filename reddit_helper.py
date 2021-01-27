581b9e57f2b8ef2b9461d979df6e01427954a8c1
rmeyer1 <robmeyer03@gmail.com>
2021-01-27 16:52:36 -0500

    Update reddit_helper.py

--- reddit_helper.py
+++ reddit_helper.py
@@ -15,7 +15,7 @@ and appended in list variable p, and then saved as pandas DataFrame object named
 """
 
 #return a dataframe for the newest reddit posts
-def get_reddit(cid= 'Lgjng9yfw7Tgew', csec= 'E8ivymEjva1p6FmczIxgWKKrsOQVZg', uag= 'bbg-dashboard', subreddit='wallstreetbets'):
+def get_reddit(cid= '', csec= '', uag= '', subreddit='wallstreetbets'):
     reddit = praw.Reddit(client_id= cid, client_secret= csec, user_agent= uag)
 
     posts = reddit.subreddit(subreddit).new(limit=None)
